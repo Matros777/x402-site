b681c350243d6db1fe2dfb93832f5e9128861fd7
Matros777 <matros@github.com>
2026-07-29 16:37:57 +0300

    fix: improve chat proxy headers

--- functions/chat.js
+++ functions/chat.js
@@ -1,10 +1,19 @@
 export async function onRequest(context) {
   const request = context.request;
   const url = new URL(request.url);
-  const target = 'https://91.149.187.50.nip.io:444' + url.pathname;
-  return fetch(target, {
+  
+  // Проксируем на наш сервер
+  const target = 'https://91.149.187.50.nip.io:444' + url.pathname + url.search;
+  
+  // Копируем заголовки, убираем врапперовые
+  const headers = new Headers(request.headers);
+  headers.set('Host', '91.149.187.50.nip.io');
+  
+  const response = await fetch(target, {
     method: request.method,
-    headers: request.headers,
+    headers: headers,
     body: request.body,
   });
+  
+  return response;
 }
