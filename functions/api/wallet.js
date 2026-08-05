5e80e6bf3044bb666c011d6610392066484f480f
Matros777 <matros@github.com>
2026-08-05 22:33:27 +0300

    Feat: wallet page - remove Optimism, unify header/footer with tester

--- functions/api/wallet.js
+++ functions/api/wallet.js
@@ -2,7 +2,6 @@ const NETWORKS = {
   "eth-mainnet": { rpc: "https://eth-mainnet.g.alchemy.com/v2", nft: "https://eth-mainnet.g.alchemy.com/nft/v3", explorer: "https://etherscan.io", native: "ETH", usdc: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", usdt: "0xdAC17F958D2ee523a2206206994597C13D831ec7" },
   "base-mainnet": { rpc: "https://base-mainnet.g.alchemy.com/v2", nft: "https://base-mainnet.g.alchemy.com/nft/v3", explorer: "https://basescan.org", native: "ETH", usdc: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913", usdt: "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2" },
   "arb-mainnet": { rpc: "https://arb-mainnet.g.alchemy.com/v2", nft: "https://arb-mainnet.g.alchemy.com/nft/v3", explorer: "https://arbiscan.io", native: "ETH", usdc: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831", usdt: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9" },
-  "opt-mainnet": { rpc: "https://opt-mainnet.g.alchemy.com/v2", nft: "https://opt-mainnet.g.alchemy.com/nft/v3", explorer: "https://optimistic.etherscan.io", native: "ETH", usdc: "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85", usdt: "0x94b008aA00579c1307B0EF2c499aD98a8ce58e58" },
   "polygon-mainnet": { rpc: "https://polygon-mainnet.g.alchemy.com/v2", nft: "https://polygon-mainnet.g.alchemy.com/nft/v3", explorer: "https://polygonscan.com", native: "MATIC", usdc: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359", usdt: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F" }
 };
 
